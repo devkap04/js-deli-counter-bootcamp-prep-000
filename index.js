f807413ab98f0d8a41532ecd4d43c57f2d05b49f
devinkapelanski <devinkapelanski@yahoo.com>
2018-09-26 04:52:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty."
   } else {
     string = `The line is currently: 1. ${katzDeliLine[0]}`
-    for(let i = 0; i < katzDeliLine.length; i++) {
+    for(let i = 1; i < katzDeliLine.length; i++) {
       string += `, ${i+1}. ${katzDeliLine[i]}`
     }
     return string;
